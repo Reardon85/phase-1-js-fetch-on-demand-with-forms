8a75bbf46d62c4090725da539f77a86dca49e8be
Brian Reardon <Brian.D.Reardon@gmail.com>
2023-03-01 06:38:48 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,39 @@
 const init = () => {
+    const form = document.querySelector('form');
+
+    form.addEventListener('submit', (event) => { 
+        event.preventDefault();
+        const book = event.target.children[1].value;
+        renderBook(book);
+
+    })
   
+}
+
+function renderBook(bookID){
+
+    const bookSection = document.querySelector("#movieDetails");
+    const title = bookSection.children[0];
+    const summary = bookSection.children[1];
+    console.log(bookID)
+    fetch("http://localhost:3000/movies")
+    .then(resp => resp.json())
+    .then(books => {
+        console.log(books)
+        books.forEach(book => { 
+            console.log(book.id)
+            if(book.id == bookID){
+                console.log("yay")
+                title.innerText = book.title;
+                summary.innerText = book.summary;
+            }
+        });
+    })
+
+    
+    //bookSection.children[0].append(title)
+    //bookSection.children[1].append(summary)
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
